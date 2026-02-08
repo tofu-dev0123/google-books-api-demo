3d8e6ea8a1971862c0ab3343e8d4bf4c8dfae340
tofu-dev0123 <m.komukai0123@gmail.com>
2026-02-08 17:27:23 +0900

    ホーム画面のUIを実装

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -1,5 +1,22 @@
-"use client"
+"use client";
+
+import Image from "next/image";
 
 export default function HomePage() {
-  return <main className="h-full w-full">Home Page!</main>;
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-center">
+      <div className="border-gray/100 rounded-md flex h-10 w-120 px-2 border justify-center">
+        <input className="border-none shadow-none outline-none focus:outline-none focus:ring-0 flex-1" type="search" placeholder="本を検索する" />
+        <div className="w-10 h-full relative flex items-center justify-center opacity-50">
+        <Image
+          src="/images/search-icon.png"
+          alt="検索"
+          width={24}
+          height={24}
+          className="my-auto"
+        />
+        </div>
+      </div>
+    </main>
+  );
 }
